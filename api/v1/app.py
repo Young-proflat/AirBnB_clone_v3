bfb8fa6ba30e69975d99aaa00b6456c2041cafef
Studio-Prince <mosesice5@gmail.com>
2024-05-01 16:53:39 -0700

    change on the app.js

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,12 +27,10 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    if getenv("HBNB_API_HOST") is None:
-        HBNB_API_HOST = '0.0.0.0'
-    else:
-        HBNB_API_HOST = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT") is None:
-        HBNB_API_PORT = 5000
-    else:
-        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app_host = getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
